77b8c4842cb44971a183e4c6e827ecef0ece8b20
Crystal Bevis <crystal.j.bevis@gmail.com>
2017-03-17 14:30:05 -0700

    Add proper file ending to whitespace.  Add practice files for apostrophes and names

---
+++ name_cases.py
@@ -0,0 +1,15 @@
+name = "Eric"
+print("Hello " + name + ", would you like to learn some Python today?")
+
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+name_first = "albert"
+name_last = "einstein"
+
+full_name = name_first.title()+ " " + name_last.title()
+
+quote = "A person who never made a mistake never tried anything new."
+
+print(full_name+" " + 'once said, "' + quote + '"')
