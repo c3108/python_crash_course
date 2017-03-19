50b5e274620af15261416453a83239013cd1536a
Crystal Bevis <crystal.j.bevis@gmail.com>
2017-03-18 19:59:47 -0700

    Modify lists.py to practice inserting and "pop"-ing

--- lists.py
+++ lists.py
@@ -1,13 +1,13 @@
 names = ['jared', ' OWEN ', 'freD', 'george ', 'frank', 'ed']
 
-print(names[0])
-
-print(names[-1])
-
 names.append('crystal')
-
-
-for name in names:	
-	print("Hello " + name.title().strip())
+names.insert(2, 'amanda')
 
 print("Goodbye!")
+
+absent = names.pop(0)
+
+print(absent.title().strip() + " can't make it anymore :-(.")
+
+for name in names:
+	print(name.title().strip() + " can make it to the party.")
