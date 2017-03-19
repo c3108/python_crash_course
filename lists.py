f95229788fa505fac219c3923027fd3624a7978c
Crystal Bevis <crystal.j.bevis@gmail.com>
2017-03-18 19:22:29 -0700

    Added additional methods to practice modifying the list

--- lists.py
+++ lists.py
@@ -1,10 +1,13 @@
-names = ['jared', 'owen', 'fred', 'george', 'frank', 'ed']
+names = ['jared', ' OWEN ', 'freD', 'george ', 'frank', 'ed']
 
 print(names[0])
 
 print(names[-1])
 
-for name in names: 
-	print("Hello " + name.title())
+names.append('crystal')
+
+
+for name in names:	
+	print("Hello " + name.title().strip())
 
 print("Goodbye!")
