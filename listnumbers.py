66f2f270d968eb57bb289af5ac3d138d610f7a59
Crystal Bevis <crystal.j.bevis@gmail.com>
2017-03-19 21:26:28 -0700

    Add test functions to listnumbers to practice generating lists and finding max and min

--- listnumbers.py
+++ listnumbers.py
@@ -7,7 +7,22 @@ squares = list(numbers)
 for index in range(len(squares)):
 	squares[index] **= 2
 
-
 print(numbers)
 print(squares)
 
+# list of even numbers
+print(list(range(2, 11, 2)))
+
+squares_again = []
+for value in range(1, 11):
+	squares_again.append(value**2)
+
+print(squares_again)
+
+
+#Simple statistics list
+
+digits = [1,2,3,4,5,6,7,8,9,0]
+print("min digit = " + str(min(digits)))
+print("max digit = " + str(max(digits)))
+print("digit sum = " + str(sum(digits)))
